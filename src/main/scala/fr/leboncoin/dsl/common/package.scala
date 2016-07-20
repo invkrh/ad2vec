2afc2f4d343684e38c8f41205a03ed68f2930dc5
Hao Ren <hao.ren@scmfrance.fr>
2016-07-21 01:04:26 +0200

    Improve test

--- src/main/scala/fr/leboncoin/dsl/common/package.scala
+++ src/main/scala/fr/leboncoin/dsl/common/package.scala
@@ -6,16 +6,18 @@ import org.apache.spark.sql.SQLContext
 import org.apache.spark.{SparkConf, SparkContext}
 
 package object common {
-
+  
+  def init() = {}
+  
   val sc = new SparkContext("local[*]", "ad2vec", new SparkConf)
-
+  
   /**
    * Efficient parquet committer
    */
   sc.hadoopConfiguration.set(
     "spark.sql.parquet.output.committer.class",
     "org.apache.spark.sql.parquet.DirectParquetOutputCommitter")
-
+  
   /**
    * AWS credential
    */
@@ -25,6 +27,6 @@ package object common {
       new SystemPropertiesCredentialsProvider)
   sc.hadoopConfiguration.set("fs.s3a.access.key", chain.getCredentials.getAWSAccessKeyId)
   sc.hadoopConfiguration.set("fs.s3a.secret.key", chain.getCredentials.getAWSSecretKey)
-
+  
   val sqlContext = new SQLContext(sc)
 }
--- src/test/scala/fr/leboncoin/dsl/Ad2VecTest.scala
+++ src/test/scala/fr/leboncoin/dsl/Ad2VecTest.scala
@@ -1,51 +1,52 @@
 package fr.leboncoin.dsl
 
-import org.apache.spark.ml.feature.{Word2Vec, Word2VecModel}
+import fr.leboncoin.dsl.common._
+import org.apache.spark.ml.feature.Word2VecModel
 import org.scalatest.FunSuite
-import common._
 
 class Ad2VecTest extends FunSuite {
-
+  
+  
   //  def prefix(tpe: String) = s"data-science-lab/word2vec/blocket/clothes/$tpe"
   def prefix(tpe: String) = s"data-science-lab/word2vec/blocket/clothes_dedup/$tpe"
-
+  
   val bucket = "data.dev.leboncoin.io-datalake"
-
+  
   val raw_prefix    = prefix("raw")
   val corpus_prefix = prefix("corpus")
   val model_prefix  = prefix("model")
   val result_prefix = prefix("result")
-
+  
   val rawURL    = s"s3a://$bucket/$raw_prefix"
   val corpusURL = s"s3a://$bucket/$corpus_prefix"
   val modelURL  = s"s3a://$bucket/$model_prefix"
   val resultURL = s"s3a://$bucket/$result_prefix"
-
+  
   test("Ad2vec should load corpus by need") {
     val df = new Ad2Vec(corpusURL).loadCorpus(removeStopWords = true, replaceNum = true)
     df.take(10) foreach println
   }
-
+  
   test("Ad2vec should convert json file to parquet file") {
     Ad2Vec.createCorpus(rawURL, corpusURL)
   }
-
+  
   test("Ad2vec should create result") {
     Ad2Vec.result(corpusURL, Some(modelURL), Some(resultURL))
   }
-
+  
   test("Checkout result") {
     val df = sqlContext.read.parquet(resultURL)
     df.show(truncate = false)
   }
-
+  
   test("Model check") {
-    sc
+    init() // init common object in which there is sc
     val model =
-      Word2VecModel.load("s3a://data.dev.leboncoin.io-datalake/data-science-lab/word2vec/model/blocket/clothes_dedup/w2v_db77e6b7a65e")
+      Word2VecModel.load(s"$modelURL/w2v_db77e6b7a65e")
     val res = model.findSynonyms("dior", 10)
     res.show()
   }
-
-
+  
+  
 }
